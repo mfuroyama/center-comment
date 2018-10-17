6a37a3de39268217a71facb8f658ea0a842de39d
Michael Furoyama <furoyama51@gmail.com>
2018-10-17 07:28:07 -1000

    Update unit tests

--- spec/atom-center-comment-spec.js
+++ spec/atom-center-comment-spec.js
@@ -1,45 +1,44 @@
 'use babel';
 
-var AtomCenterComment = require('../lib/atom-center-comment');
+import buildCommentString from '../lib/build-comment-string';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
 // To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
-describe('AtomCenterComment', function() {
-
-    beforeEach(function() {
+describe('AtomCenterComment', () => {
+    beforeEach(() => {
         atom.config.set('editor.preferredLineLength', 100);
     });
 
-    describe('Centered comment conversion', function() {
-        it('Handles single-line Javascript comments', function() {
-            expect(AtomCenterComment.buildCommentString('// -Dashes', 'js'))
+    describe('Centered comment conversion', () => {
+        it('Handles single-line Javascript comments', () => {
+            expect(buildCommentString('// -Dashes', 'js'))
                 .toBe('// ---------------------------------------------Dashes----------------------------------------------');
         });
-        it('Handles single-line Javascript comments with multi-character pad', function() {
-            expect(AtomCenterComment.buildCommentString('// =* MultiCharacter', 'js'))
+        it('Handles single-line Javascript comments with multi-character pad', () => {
+            expect(buildCommentString('// =* MultiCharacter', 'js'))
                 .toBe('// =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=* MultiCharacter =*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=');
         });
-        it('Handles multi-line Javascript comments', function() {
-            expect(AtomCenterComment.buildCommentString('/* - Single-line block comment */', 'js'))
+        it('Handles multi-line Javascript comments', () => {
+            expect(buildCommentString('/* - Single-line block comment */', 'js'))
                 .toBe('/* --------------------------------- Single-line block comment ---------------------------------- */');
         });
-        it('Handles single-line Ruby comments', function() {
-            expect(AtomCenterComment.buildCommentString('# = Ruby Test', 'rb'))
+        it('Handles single-line Ruby comments', () => {
+            expect(buildCommentString('# = Ruby Test', 'rb'))
                 .toBe('# =========================================== Ruby Test ============================================');
         });
-        it('Handles block-style HTML comments', function() {
-            expect(AtomCenterComment.buildCommentString('<!-- * HTML -->', 'html'))
+        it('Handles block-style HTML comments', () => {
+            expect(buildCommentString('<!-- * HTML -->', 'html'))
                 .toBe('<!-- ****************************************** HTML ******************************************* -->');
         });
-        it('Handles spaces in front of comments', function() {
-            expect(AtomCenterComment.buildCommentString('    // = Equals', 'js'))
+        it('Handles spaces in front of comments', () => {
+            expect(buildCommentString('    // = Equals', 'js'))
                 .toBe('    // ========================================== Equals ===========================================');
         });
-        it('Ignores non comment lines', function() {
-            expect(AtomCenterComment.buildCommentString('var f = 100;', 'js')).toBeNull();
+        it('Ignores non comment lines', () => {
+            expect(buildCommentString('var f = 100;', 'js')).toBeNull();
         });
     });
 });
